89c940aec42b3d9cf4d759112112740cf85ddd24
ReineMoberg <reine.moberg3@gmail.com>
2021-02-19 09:29:35 +0100

    Implemented create() method in PeopleImpl.

--- src/main/java/se/lexicon/group_Reine/App.java
+++ src/main/java/se/lexicon/group_Reine/App.java
@@ -1,7 +1,10 @@
 package se.lexicon.group_Reine;
 
 
+import se.lexicon.group_Reine.data.People;
+import se.lexicon.group_Reine.data.PeopleImpl;
 import se.lexicon.group_Reine.data.database.MySqlConnection;
+import se.lexicon.group_Reine.model.Person;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,12 +15,19 @@ public class App
     public static void main( String[] args )
     {
         //Test connection to MySQL database
-        Connection connection = MySqlConnection.getConnection();
+        /*Connection connection = MySqlConnection.getConnection();
         try {
             connection.close();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }
+        }*/
+
+        //Test add person(s) to database
+        People people = new PeopleImpl();
+        Person person1 = people.create(new Person("Reine", "Moberg"));
+        Person person2 = people.create(new Person("Anna", "Karlsson"));
+        System.out.println(person1);
+        System.out.println(person2);
 
 
     }
